f95f39d2b8a352384de9ab5e00fc9df3bae35442
吴守雍 <872789604@qq.com>
2021-04-15 23:55:57 +0800

    index add categories

--- src/api/topic.js
+++ src/api/topic.js
@@ -1,7 +1,16 @@
 import {request} from '@/utils/request'
 
+/**
+ * 话题列表
+ */
 export function getTopics(data) {
   return request('topics', {
     data: data
   })
+}
+/**
+ * 话题分类
+ */
+export function getCategories(data) {
+  return request('categories')
 }
