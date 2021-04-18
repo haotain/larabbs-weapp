fa598df677a5cb5f2b62ff4da7eff99a4af7390b
吴守雍 <872789604@qq.com>
2021-04-18 16:38:45 +0800

    page reply create

--- src/api/reply.js
+++ src/api/reply.js
@@ -1,13 +1,27 @@
 import { request, authRequest } from '@/utils/request'
 
+/**
+ * 获取话题回复
+ */
 export function getReplies(topicId, data) {
     return request('topics/' + topicId + '/replies', {
         data: data
     })
 }
-
+/**
+ * 获取某用户的回复
+ */
 export function getUserReplies(userId, data) {
     return request('users/' + userId + '/replies', {
+        data: data
+    })
+}
+/**
+ * 回复话题
+ */
+export function createReply(topicId, data) {
+    return authRequest('topics/' + topicId + '/replies', {
+        method: 'post',
         data: data
     })
 }
