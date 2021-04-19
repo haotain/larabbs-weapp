d19ec01948bc5d897ddd1fa461cff59c0d87551b
吴守雍 <872789604@qq.com>
2021-04-19 14:18:01 +0800

    page notification index

--- src/api/notification.js
+++ src/api/notification.js
@@ -1,5 +1,15 @@
 import {authRequest} from '@/utils/request'
 
+/**
+ * 获取未读消息
+ */
 export function getNotificationStats(...params) {
   return authRequest('notifications/stats', ...params)
+}
+
+/**
+ * 消息列表
+ */
+export function getNotifications() {
+  return authRequest('notifications')
 }
